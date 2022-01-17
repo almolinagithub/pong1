89bd8c17bd626a845252a6e12abdb51715918b16
Noodles Florence <alessandromolinari2000@hotmail.com>
2022-01-17 22:36:06 +0100

    ball added

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-import turtle
 import time
-import paddles
+import turtle
+from ball import Ball
+from paddles import Paddle
+from time import sleep
 
 PLAYING  = True
 
-width = 800
 height = 600
+width = 800
 x = 330
 y = 0
+
 
 screen = turtle.Screen()
 screen.setup(width, height)
@@ -17,21 +20,40 @@ screen.title("pong")
 screen.tracer(0)
 screen.listen()
 
+#top and bottom of the screen
+top = screen.window_height() / 2
+right = screen.window_width() / 2
+bottom = - top
+left = - right
 
-paddle_right = paddles.Paddle()
-paddle_left = paddles.Paddle()
+#making the paddles
+paddle_right = Paddle((x, 0))
+paddle_left = Paddle((-x, 0))
 
-paddle_right.goto(x,y)
-paddle_left.goto(-x,0)
+#set paddles positions
+new_x = paddle_right.xcor() + 330
+paddle_right.goto(new_x,y)
+new_x = paddle_left.xcor() - 330
+paddle_left.goto(new_x,y)
 
+#moving the paddles
 turtle.onkey(paddle_right.move_right, "Up")
 turtle.onkey(paddle_right.move_left, "Down")
-
 turtle.onkey(paddle_left.move_right, "a")
 turtle.onkey(paddle_left.move_left, "z")
 
+#crate the ball
+ball = Ball()
+
+
 while  PLAYING:
+    time.sleep(0.1)
     screen.update()
+    screen.tracer(1)
+    ball.ball_move()
+
+
+
 
 screen.exitonclick()
 
